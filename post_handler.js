643ce74e2dfdfa8cf51993dccb591807ccf1b989
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2020-03-27 20:46:17 -0500

    pattern redux and post handler

--- post_handler.js
+++ post_handler.js
@@ -9,16 +9,24 @@ $(document).ready(function () {
             data: {
                 ceriba: {
                     water: $('#water').val(),
-                    link: $('#link').val(),
-                    imageUrl: $('#imageUrl').val(),
-                    description: $('#description').val(),
+                    state: $('#state').val(),
+                    business_city: $('#business_city').val(),
+                    address: $('#address').val(),
+                    phone: $('#business_contact_number').val(),
+                    store: $('#business_name').val(),
+                    meats: $('#meat').val(),
+                    city: $('#business_city').val(),
+                    water: $('#water').val(),
+                    water: $('#water').val(),
+                    water: $('#water').val(),
+                    water: $('#water').val(),
                 }
             },
             success: function () {
                 $('#success').show();
             },
             error: function (xhr, res, text) {
-console.log("An Error Occurred")
+            console.log("An Error Occurred")
             }
         });
     });
