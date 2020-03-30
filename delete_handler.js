d125193c6a693e0098130932ca70f3637eeb41b1
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2020-03-29 20:16:34 -0500

    BB3

---
+++ delete_handler.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $('#deleteItem').submit(function (e) {
+        e.preventDefault();
+        let form = $(this);
+        $.ajax({
+            type: 'delete',
+            dataType: 'json',
+            url: form.attr('action'),
+            data: {
+                ceriba: {
+                    emtuar: $('#water').is(':checked'),
+                    state: $('#state').val(),
+                    business_city: $('#business_city').val(),
+                    address: $('#address').val(),
+                    phone: $('#business_contact_number').val(),
+                    store: $('#business_name').val(),
+                    jmsdeu: $('#meat').is(':checked'),
+                    location: $('#business_city').val(),
+                    timestamp: $('#timeS').val(),
+                    jplqwnrt: $('#toilet').is(':checked'),
+                    jzeurm: $('#mask').is(':checked'),
+                    uxyzqio: $('#cleaning').is(':checked'),
+                    poyunz: $('#chicken').is(':checked'), 
+                }
+            },
+            success: function () {
+                $('#success').show();
+                $('#post').hide();
+            },
+            error: function (xhr, res, text) {
+            console.log("An Error Occurred");
+        
+            }
+        });
+    });
+});
